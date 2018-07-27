57aaff46a6b7e5a1f0faed2254f98e0bfa824d4c
Rakesh Singh <rs414057@gmail.com>
2018-07-27 21:36:16 +0530

    Update usb_logs.py

--- usb_logs.py
+++ usb_logs.py
@@ -1,8 +1,27 @@
-import platform
-if (platform.system()=="Linux"):
-    #usb logs history stored in /var/log/messages
-    with open("/var/log/messages") as f:
-        for lines in f:
-            if "usb" in lines:
-                print lines
+from _winreg import *
+import os
+
+def identify_os():
+    if(os.name=="nt"):
+        """print r"*** Reading usb logs history ***" """
+        aReg = ConnectRegistry(None, HKEY_LOCAL_MACHINE)
+
+        aKey = OpenKey(aReg, r"SYSTEM\CurrentControlSet\Enum\USBSTOR")
+        for i in range(1024):
+            try:
+                asubkey_name = EnumKey(aKey, i)
+                print asubkey_name
+               
+            except EnvironmentError:
+                break
+    elif(os.name=="linux"):
+        with open("/var/log/message") as fh:
+            for line in fh:
+                if("usb" in line):
+                    print "line of usb",line
+
+
+
+
+identify_os()
 
